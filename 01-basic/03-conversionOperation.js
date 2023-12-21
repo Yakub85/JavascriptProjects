079b5302c45af44bb7c9cf1aaf588287832d1e78
Yakub85 <68981086+Yakub85@users.noreply.github.com>
2023-12-21 14:29:09 +0000

    Conversion of Datatype

---
+++ 01-basic/03-conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abn"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33"=>33
+//"33fs"=> NaN
+//true => 1; false =>0
+
+let isLoggedIn = "yaqub"
+
+let booleeanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleeanIsLoggedIn);
+
+//1=>true; 0=>false
+// ""=>false
+//"yaqub"=>true
+
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
