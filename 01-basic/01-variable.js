9a0fba83184d3784c8c1992cf98469d0ad56be61
Yakub85 <68981086+Yakub85@users.noreply.github.com>
2023-12-21 07:24:26 +0000

    container config done

---
+++ 01-basic/01-variable.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="yaqub@gmail.com"
+var accountPassword= "12345"
+accountCity="Lucknow"
+
+//accountId= 2 //changes not allowed
+
+accountEmail="yakub@gmail.com"
+accountPassword="123"
+accountCity="Deoria"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
