4eb130e38339b44221de7e75105d3157d50de308
Yakub85 <68981086+Yakub85@users.noreply.github.com>
2023-12-22 17:25:03 +0000

    conversionOperation done

--- 01-basic/03-conversionOperation.js
+++ 01-basic/03-conversionOperation.js
@@ -15,7 +15,7 @@ let score = "33abn"
 let isLoggedIn = "yaqub"
 
 let booleeanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleeanIsLoggedIn);
+// console.log(booleeanIsLoggedIn);
 
 //1=>true; 0=>false
 // ""=>false
@@ -23,7 +23,34 @@ console.log(booleeanIsLoggedIn);
 
 let someNumber=33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 //===========Operation=========
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(3*5);
+// console.log(5-3);
+// console.log(11/5);
+// console.log(2%3);
+// console.log(3**2);
+
+let str1="Yaqub "
+let str2="Ansari"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%3);
+
+console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
