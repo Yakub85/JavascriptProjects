2b7f566b197a092155b79c9dc4d6ad97d470289a
Yakub Ansari <68981086+Yakub85@users.noreply.github.com>
2024-08-18 08:08:03 +0000

    bank app done

--- js/Bank/script.js
+++ js/Bank/script.js
@@ -21,9 +21,25 @@ class Bank{
 }
 
 const bank=new Bank(100)
-console.log(bank.balance);
-bank.deposit(200)
-bank.withdraw(100)
-bank.withdraw(100)
-bank.withdraw(50)
+// console.log(bank.balance);
+// bank.deposit(200)
+// bank.withdraw(100)
+// bank.withdraw(100)
+// bank.withdraw(50)
 
+const amountInput=document.getElementById('amount-input');
+const Balance=document.getElementById('balance')
+
+const depositBtn=document.getElementById('deposit')
+depositBtn.onclick = ()=>{
+    const amount=Number(amountInput.value)
+    bank.deposit(amount);
+    Balance.innerText=`Balance:₹${bank.balance}`
+}
+
+const withdrawBtn=document.getElementById('withdraw')
+withdrawBtn.onclick = ()=>{
+    const amount=Number(amountInput.value)
+    bank.withdraw(amount);
+    Balance.innerText=`Balance:₹${bank.balance}`;
+}
